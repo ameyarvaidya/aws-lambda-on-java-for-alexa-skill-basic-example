0bbcc4f7e542faa6ada79e4ba047dcd98d4f4cdd
Sergey S <ssmtor@gmail.com>
2018-04-01 01:42:31 +0200

    Initial source commit

---
+++ sources/src/MagicDiceHandler.java
@@ -0,0 +1,31 @@
+import com.amazon.ask.Skill;
+import com.amazon.ask.builder.StandardSkillBuilder;
+import com.amazon.ask.model.RequestEnvelope;
+import com.amazon.ask.model.ResponseEnvelope;
+import com.amazon.ask.model.services.Serializer;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+
+public class MagicDiceHandler implements RequestStreamHandler {
+    private final Skill skill;
+
+    public MagicDiceHandler() {
+        skill = new StandardSkillBuilder()
+                .addRequestHandler(new RandomNumberHandler())
+                .build();
+    }
+
+    @Override
+    public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
+        Serializer serializer = new com.amazon.ask.util.JacksonSerializer();
+        String serializedRequest = org.apache.commons.io.IOUtils.toString(input, StandardCharsets.UTF_8);
+        RequestEnvelope requestEnvelope = serializer.deserialize(serializedRequest, RequestEnvelope.class);
+        ResponseEnvelope response = skill.invoke(requestEnvelope);
+        byte[] serializedResponse = serializer.serialize(response).getBytes(StandardCharsets.UTF_8);
+        output.write(serializedResponse);
+    }
+}
